dc2a9de590b477ad70cd97fda6de4275d3e20d55
Julien Benincasa <julien.benincasa@itesoft.com>
2023-10-24 11:03:21 +0200

    Refactor + useless style rule removed

--- js/dashboard_script.js
+++ js/dashboard_script.js
@@ -2,27 +2,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('start');
     const stopButton = document.getElementById('stop');
 
+    const activate = (bool) => {
+        startButton.style.display = bool === true ? 'none' : 'block';
+        stopButton.style.display = bool === true ? 'block' : 'none';
+    }
+
     chrome.action.getBadgeText({}, (badgeText) => {
         if (badgeText === 'ON') {
-            startButton.style.display = 'none';
-            stopButton.style.display = 'block';
+            activate(true);
         } else if (badgeText === 'OFF') {
-            startButton.style.display = 'block';
-            stopButton.style.display = 'none';
+            activate(false);
         }
     });
   
     startButton.addEventListener('click', async () => {
         const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
         await chrome.action.setBadgeText({ text: 'ON' });
-        startButton.style.display = 'none';
-        stopButton.style.display = 'block';
+        activate(true);
     });
 
     stopButton.addEventListener('click', async () => {
         const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
         await chrome.action.setBadgeText({ text: 'OFF' });
-        stopButton.style.display = 'none';
-        startButton.style.display = 'block';
+        activate(false);
     });
 });
