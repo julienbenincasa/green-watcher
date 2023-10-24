43d0cdea451fda4b488b0db5a966c2a0201acb76
Julien Benincasa <julien.benincasa@itesoft.com>
2023-10-24 09:26:49 +0200

    State & CSS changes

---
+++ js/dashboard_script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.getElementById('start');
+    const stopButton = document.getElementById('stop');
+  
+    startButton.addEventListener('click', async () => {
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        await chrome.action.setBadgeText({ tabId: tab.id, text: 'ON' });
+        startButton.style.display = 'none';
+        stopButton.style.display = 'block';
+    });
+
+    stopButton.addEventListener('click', async () => {
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        await chrome.action.setBadgeText({ tabId: tab.id, text: 'OFF' });
+        stopButton.style.display = 'none';
+        startButton.style.display = 'block';
+    });
+});
