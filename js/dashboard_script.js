23478660aaf08c8d5dbac8d4accc76d4b0671555
Julien Benincasa <julien.benincasa@itesoft.com>
2023-10-24 10:44:46 +0200

    Switch button watches extension state

--- js/dashboard_script.js
+++ js/dashboard_script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('start');
     const stopButton = document.getElementById('stop');
+
+    chrome.action.getBadgeText({}, (badgeText) => {
+        if (badgeText === 'ON') {
+            startButton.style.display = 'none';
+            stopButton.style.display = 'block';
+        } else if (badgeText === 'OFF') {
+            startButton.style.display = 'block';
+            stopButton.style.display = 'none';
+        }
+    });
   
     startButton.addEventListener('click', async () => {
         const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
