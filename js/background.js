1d8a8a8599831f504c2acf36bdf0b8cb9e501e5d
Lucas Cassan <lucascassan@outlook.com>
2023-10-24 11:44:20 +0200

    calcule poids + temps requête

--- js/background.js
+++ js/background.js
@@ -7,9 +7,36 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-chrome.webRequest.onCompleted.addListener(
-  (details) => {
-    console.log("onCompleted" + details.url);
+const activeRequests = {};
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    activeRequests[details.requestId] = { startTime: performance.now() };
   },
-  { urls: ["<all_urls>"] }
+  { urls: ["<all_urls>"] },
+  ["requestBody"]
+);
+
+chrome.webRequest.onCompleted.addListener(
+  function (details) {
+    const requestInfo = activeRequests[details.requestId];
+    if (requestInfo) {
+      const endTime = performance.now();
+      const requestTime = endTime - requestInfo.startTime;
+      const responseHeaders = details.responseHeaders;
+      let responseSize = 0;
+
+      for (const header of responseHeaders) {
+        if (header.name.toLowerCase() === "content-length") {
+          responseSize = parseInt(header.value);
+          break;
+        }
+      }
+      console.log(
+        `Requête ${details.url}, Temps de requête : ${requestTime} ms, Poids de la réponse : ${responseSize} octets.`
+      );
+      delete activeRequests[details.requestId];
+    }
+  },
+  { urls: ["<all_urls>"] },
+  ["responseHeaders"]
 );
