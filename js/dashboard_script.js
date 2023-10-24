2649532d8522b0b70c738d12fb18f915de4bc5d6
Lucas Cassan <lucascassan@outlook.com>
2023-10-24 12:09:14 +0200

    update logo header

--- js/dashboard_script.js
+++ js/dashboard_script.js
@@ -1,32 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const startButton = document.getElementById("start");
-    const stopButton = document.getElementById("stop");
+  const startButton = document.getElementById("start");
+  const stopButton = document.getElementById("stop");
+  const logoOn = document.getElementById("logoOn");
+  const logoOff = document.getElementById("logoOff");
 
-    const activate = (bool) => {
-        chrome.storage.session.set({ extensionState: bool === true ? "ON" : "OFF" });
-        startButton.style.display = bool === true ? "none" : "block";
-        stopButton.style.display = bool === true ? "block" : "none";
-    };
-
-    chrome.storage.session.get(["extensionState"]).then((data) => {
-        data["extensionState"] === "ON" ? activate(true) : activate(false);
+  const activate = (bool) => {
+    chrome.storage.session.set({
+      extensionState: bool === true ? "ON" : "OFF",
     });
+    startButton.style.display = bool === true ? "none" : "block";
+    stopButton.style.display = bool === true ? "block" : "none";
+    logoOff.style.display = bool === true ? "none" : "block";
+    logoOn.style.display = bool === true ? "block" : "none";
+  };
 
-    startButton.addEventListener("click", async () => {
-        activate(true);
-        await chrome.action.setIcon({
-            path: {
-            96: "../img/logo/logo_ico.png",
-            },
-        });
-    });
+  chrome.storage.session.get(["extensionState"]).then((data) => {
+    data["extensionState"] === "ON" ? activate(true) : activate(false);
+  });
 
-    stopButton.addEventListener("click", async () => {
-        activate(false);
-        await chrome.action.setIcon({
-        path: {
-            96: "../img/logo/logo_red_ico.png",
-        },
-        });
+  startButton.addEventListener("click", async () => {
+    activate(true);
+    await chrome.action.setIcon({
+      path: {
+        96: "../img/logo/logo_ico.png",
+      },
     });
+    logoOn.add;
+  });
+
+  stopButton.addEventListener("click", async () => {
+    activate(false);
+    await chrome.action.setIcon({
+      path: {
+        96: "../img/logo/logo_red_ico.png",
+      },
+    });
+  });
 });
